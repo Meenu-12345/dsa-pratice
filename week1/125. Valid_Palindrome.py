1ead936e7a19092f4fe7c4579d084b8d6195e734
Meenu Singh <meenusingh8532@gmail.com>
2026-03-28 14:25:37 +0530

    add isPalindrome function to check if a string is a palindrome

---
+++ week1/125. Valid_Palindrome.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = "A man, a plan, a canal: Panama"
+     
+        #Two Pointer - Optimal
+        left = 0s
+        right = len(s) - 1
+        
+        while left < right:
+            
+            # Skip non-alphanumeric from left
+            while left < right and not s[left].isalnum():
+                left += 1
+            
+            # Skip non-alphanumeric from right
+            while left < right and not s[right].isalnum():
+                right -= 1
+            
+            # Compare characters (case-insensitive)
+            if s[left].lower() != s[right].lower():
+                return False
+            
+            left += 1
+            right -= 1
+        
+        return True
+        
